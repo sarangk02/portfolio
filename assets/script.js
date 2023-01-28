7e466a24fa29f4eac2a5fa6c2544742722b87069
Sarang Kulkarni <sarangakulkarni02@gmail.com>
2023-01-28 13:31:44 +0530

    nearly end

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,15 @@
 
 function openmenu() {
     document.getElementById("menu-container").style.display = 'block';
-    document.getElementById("non-menu").style.display = 'none';
     document.getElementById("scrolldowngif").style.display = 'none';
+    document.getElementById("blankside").style.display = 'block';
     document.getElementById('html').style.overflow = 'hidden';
 }
 
 function closemenu() {
     document.getElementById("menu-container").style.display = 'none';
-    document.getElementById("non-menu").style.display = 'block';
     document.getElementById("scrolldowngif").style.display = 'block';
+    document.getElementById("blankside").style.display = 'none';
     document.getElementById('html').style.overflow = 'scroll';
     window.scroll(0, 0); 
 }
